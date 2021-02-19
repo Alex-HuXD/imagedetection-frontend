abc89f54f80738c6cc62348b80c27610f8907b19
Alex Hu <xindi_hu@yahoo.com>
2021-02-19 15:06:12 -0600

    fix route

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,7 +5,7 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
     return (
       <nav style={{ display: "flex", justifyContent: "flex-end" }}>
         <p
-          onClick={() => onRouteChange("signin")}
+          onClick={() => onRouteChange("signout")}
           className="f3 link dim black underline pa3 pointer"
         >
           Sign out
